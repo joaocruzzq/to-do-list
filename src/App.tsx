b2af83435149bfe9e2f9c18e043806c54cdf65b4
joão pedro cruz <joaocruzzdev@gmail.com>
2025-01-15 19:25:27 -0300

    componente tasksList

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import { Header } from './components/header'
 import { Input } from './components/input'
 import { ButtonCreateTask } from './components/buttonCreateTask'
 
+import { ClipboardText } from 'phosphor-react'
+
 export function App() {
   return (
     <div>
@@ -13,6 +15,29 @@ export function App() {
       <div className={styles.addNewTask}>
         <Input />
         <ButtonCreateTask />
+      </div>
+
+      <div className={styles.tasksList}>
+        <header>
+          <div className={styles.task}>
+            <strong className={styles.created}>Tarefas criadas</strong>
+            <span>0</span>
+          </div>
+
+          <div className={styles.task}>
+            <strong className={styles.done}>Concluídas</strong>
+            <span>0</span>
+          </div>
+        </header>
+
+        <main>
+          <ClipboardText size={58} />
+
+          <span>
+            <strong>Você ainda não tem tarefas cadastradas</strong>
+            <br /> Crie tarefas e organize seus itens a fazer
+          </span>
+        </main>
       </div>
     </div>
   )
